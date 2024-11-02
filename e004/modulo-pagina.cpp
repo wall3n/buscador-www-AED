2eef02d3ffeacd6de209eb669e2121bf6b1b1b0e
Wall3n <franmoreno1506@gmail.com>
2024-11-02 19:26:19 +0100

    Refac: Implementacion de las clases amigas para eliminar metodos get y set

--- e004/modulo-pagina.cpp
+++ e004/modulo-pagina.cpp
@@ -11,22 +11,3 @@ void Pagina::escribir(){
     cout << url << ", " << titulo << ", " << "Rel. " << relevancia << endl;
 }
 
-string Pagina::getUrl(){
-    return url;
-}
-
-string Pagina::getTitulo(){
-    return this->titulo;
-}
-
-int Pagina::getRelevancia(){
-    return this->relevancia;
-}
-
-void Pagina::setTitulo(string nuevo){
-    this->titulo = nuevo;
-}
-
-void Pagina::setRelevancia(int nueva){
-    this->relevancia = nueva;
-}
