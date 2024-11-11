1f6af98180fef7227de4cdb7df5f40e8e005b10e
Wall3n <franmoreno1506@gmail.com>
2024-11-11 14:58:58 +0100

    Feat: Ficheros Base del ejercicio e004

---
+++ e200/modulo-pagina.cpp
@@ -0,0 +1,13 @@
+#include "modulo-pagina.hpp"
+
+void Pagina::leer(){
+    cin >> relevancia;
+    cin.ignore();
+    getline(cin, url);
+    getline(cin, titulo);
+}
+
+void Pagina::escribir(){
+    cout << url << ", " << titulo << ", " << "Rel. " << relevancia << endl;
+}
+
