f94807cfcdd1a23eef7a2a94832a46bd35483842
Wall3n <franmoreno1506@gmail.com>
2024-11-13 19:31:36 +0100

    Feat: Implementacion del Arbol Trie en el diccionario de paginas

---
+++ e300/modulo-dicpaginas.cpp
@@ -0,0 +1,35 @@
+#include "modulo-dicpaginas.hpp"
+#include "modulo-arbol.hpp"
+#include "modulo-tablahash.hpp"
+
+DicPaginas::DicPaginas(){
+    tabla = new TablaHash();
+    arbol = new ArbolPalabras();
+}
+
+DicPaginas::~DicPaginas(){
+    delete tabla;
+    delete arbol;
+}
+
+int DicPaginas::tamano(){
+    return tabla->numElementos();
+}
+
+void DicPaginas::insertar(Pagina nueva){
+    tabla->insertar(nueva);
+}
+
+Pagina* DicPaginas::consultar(string url){
+    return tabla->consultar(url);
+}
+
+void DicPaginas::insertar(string palabra, Pagina * pag){
+    return this->arbol->insertar(palabra, pag);
+}
+
+list<Pagina *> DicPaginas::buscar(string palabra){
+    return this->arbol->buscar(palabra);
+}
+
+
---
+++ e300/modulo-dicpaginas.hpp
@@ -0,0 +1,27 @@
+#ifndef __MODULO_DICPAGINAS__
+#define __MODULO_DICPAGINAS__
+
+#include <list>
+
+#include "modulo-arbol.hpp"
+#include "modulo-pagina.hpp"
+#include "modulo-tablahash.hpp"
+
+
+class DicPaginas {
+
+    private:
+        TablaHash * tabla;
+        ArbolPalabras * arbol;
+    public:
+        DicPaginas();
+        ~DicPaginas();
+        void insertar(Pagina nueva);
+        Pagina* consultar(string url);
+        void insertar(string palabra, Pagina * pag);
+        list<Pagina*> buscar(string palabra);
+        int tamano(void);
+};
+
+
+#endif
