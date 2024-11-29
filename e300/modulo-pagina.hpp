d88bd91aaa6d72b68e42d7789fa1a093cb846ee7
Wall3n <franmoreno1506@gmail.com>
2024-11-29 13:23:44 +0100

    Fix: Clase amiga arbol paginas para poder acceder a sus atributos privados

--- e300/modulo-pagina.hpp
+++ e300/modulo-pagina.hpp
@@ -1,13 +1,14 @@
 #ifndef __MODULO_PAGINA__
 #define __MODULO_PAGINA__
+#include "modulo-arbol.hpp"
 #include "modulo-tablahash.hpp"
 #include <iostream>
 
 using namespace std;
 
 class Pagina {
-    friend class DicPaginas;
     friend class TablaHash;
+    friend class ArbolPalabras;
     private:
         int relevancia;
         string url, titulo;
